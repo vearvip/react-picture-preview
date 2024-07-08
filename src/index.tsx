a6d79544e025b3129c705bf3817eb0893f13aaa7
唯二 <vear.vip@qq.com>
2024-07-08 09:47:15 +0000

    0.2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Image from "rc-image";
 import "rc-image/assets/index.css";
 import {
@@ -9,25 +9,24 @@ import {
   ZoomInOutlined,
   ZoomOutOutlined,
   DownloadOutlined,
+  LeftOutlined,
+  RightOutlined,
 } from "@ant-design/icons";
 import { createRoot } from "react-dom/client";
 
-/**
- * PhotoGallery组件渲染一个图片查看器。
- *
- * @param {Object} props - 组件的参数
- * @param {string} props.src - 图片的URL
- * @param {Function} [props.onClose] - 图片查看器关闭时的回调函数
- */
-const PhotoGallery = ({ src, onClose }) => {
-  const [visible, setVisible] = useState(true);
+// 定义组件的 Props 类型
+interface PhotoGalleryProps {
+  src: string;
+  images?: string[];
+  onClose?: () => void;
+}
 
-  /**
-   * 处理图片的下载功能。
-   * @async
-   * @function
-   * @returns {Promise<void>}
-   */
+const PhotoGallery: React.FC<PhotoGalleryProps> = ({ src, images, onClose }) => {
+  const [visible, setVisible] = useState(true);
+  const [current, setCurrent] = useState(
+    (images || []).findIndex((image) => image === src) || 0,
+  )
+
   const handleDownload = async () => {
     try {
       const response = await fetch(src);
@@ -36,7 +35,7 @@ const PhotoGallery = ({ src, onClose }) => {
       const a = document.createElement("a");
       a.style.display = "none";
       a.href = url;
-      a.download = src.split("/").pop(); // 使用图片文件名
+      a.download = src.split("/").pop()!; // 使用图片文件名
       document.body.appendChild(a);
       a.click();
       window.URL.revokeObjectURL(url);
@@ -47,59 +46,52 @@ const PhotoGallery = ({ src, onClose }) => {
   };
 
   return (
-    <Image
-      src={src}
-      preview={{
-        visible,
-        onVisibleChange: (visible) => {
-          setVisible(visible);
-          if (!visible) {
-            onClose && onClose();
-          }
-        },
-        icons: {
-          close: <CloseOutlined />,
-        },
-        toolbarRender: (
-          _,
-          {
-            actions: {
-              onFlipY,
-              onFlipX,
-              onRotateLeft,
-              onRotateRight,
-              onZoomOut,
-              onZoomIn,
-            },
+    (images && Array.isArray(images) && images.length > 0)
+      ?
+      <Image.PreviewGroup
+        preview={{
+          visible,
+          current: current,
+          onVisibleChange: (visible: boolean) => {
+            setVisible(visible);
+            if (!visible && onClose) {
+              onClose();
+            }
           },
-        ) => {
-          const buttons = {
-            rotateLeft: <RotateLeftOutlined onClick={onRotateLeft} />,
-            rotateRight: <RotateRightOutlined onClick={onRotateRight} />,
-            zoomIn: <ZoomInOutlined onClick={onZoomIn} />,
-            zoomOut: <ZoomOutOutlined onClick={onZoomOut} />,
-            flipX: <SwapOutlined onClick={onFlipX} />,
-            flipY: <SwapOutlined rotate={90} onClick={onFlipY} />,
-            download: (
-              <DownloadOutlined key="download" onClick={handleDownload} />
-            ),
-          };
-          return (
-            <div
-              className="rc-image-preview-footer"
-              style={{
-                position: "fixed",
-                zIndex: "1001",
-                bottom: "32px",
-                left: "0",
-                width: "100%",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
+          onChange: (current: number) => {
+            setCurrent(current);
+          },
+          icons: {
+            close: <CloseOutlined />,
+            left: <LeftOutlined />,
+            right: <RightOutlined />
+          },
+          toolbarRender: (
+            _,
+            {
+              actions: {
+                onFlipY,
+                onFlipX,
+                onRotateLeft,
+                onRotateRight,
+                onZoomOut,
+                onZoomIn,
+              },
+            }
+          ) => {
+            const buttons = {
+              rotateLeft: <RotateLeftOutlined onClick={onRotateLeft} />,
+              rotateRight: <RotateRightOutlined onClick={onRotateRight} />,
+              zoomIn: <ZoomInOutlined onClick={onZoomIn} />,
+              zoomOut: <ZoomOutOutlined onClick={onZoomOut} />,
+              flipX: <SwapOutlined onClick={onFlipX} />,
+              flipY: <SwapOutlined rotate={90} onClick={onFlipY} />,
+              download: (
+                <DownloadOutlined key="download" onClick={handleDownload} />
+              ),
+            };
+            return (
               <div
-                className="rc-image-preview-operations"
                 style={{
                   display: "flex",
                   color: "#bbb",
@@ -122,39 +114,115 @@ const PhotoGallery = ({ src, onClose }) => {
                   </div>
                 ))}
               </div>
-            </div>
-          );
-        },
-      }}
-      style={{
-        display: "none",
-      }}
-    />
+            );
+          },
+        }}
+      >
+        {
+          images.map(src => {
+            return <Image
+              key={src}
+              src={src}
+              style={{
+                display: "none",
+              }} />
+          })
+        }
+      </Image.PreviewGroup>
+      : <Image
+        src={src}
+        preview={{
+          visible,
+          onVisibleChange: (visible: boolean) => {
+            setVisible(visible);
+            if (!visible && onClose) {
+              onClose();
+            }
+          },
+          icons: {
+            close: <CloseOutlined />,
+          },
+          toolbarRender: (
+            _,
+            {
+              actions: {
+                onFlipY,
+                onFlipX,
+                onRotateLeft,
+                onRotateRight,
+                onZoomOut,
+                onZoomIn,
+              },
+            }
+          ) => {
+            const buttons = {
+              rotateLeft: <RotateLeftOutlined onClick={onRotateLeft} />,
+              rotateRight: <RotateRightOutlined onClick={onRotateRight} />,
+              zoomIn: <ZoomInOutlined onClick={onZoomIn} />,
+              zoomOut: <ZoomOutOutlined onClick={onZoomOut} />,
+              flipX: <SwapOutlined onClick={onFlipX} />,
+              flipY: <SwapOutlined rotate={90} onClick={onFlipY} />,
+              download: (
+                <DownloadOutlined key="download" onClick={handleDownload} />
+              ),
+            };
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  color: "#bbb",
+                  background: "rgba(0, 0, 0, 0.45)",
+                  borderRadius: "100px",
+                  padding: "0 20px",
+                }}
+              >
+                {Object.keys(buttons).map((key, index) => (
+                  <div
+                    key={key}
+                    style={{
+                      padding: "10px",
+                      cursor: "pointer",
+                      marginLeft: index === 0 ? 0 : "10px",
+                      fontSize: "18px",
+                    }}
+                  >
+                    {buttons[key]}
+                  </div>
+                ))}
+              </div>
+            );
+          },
+        }}
+        style={{
+          display: "none",
+        }}
+      />
   );
 };
 
-/**
- * 预览图片函数，显示图片查看器。
- *
- * @param {Object} options - 预览选项
- * @param {string} options.src - 要预览的图片URL
- * @param {Function} [options.onClose] - 图片查看器关闭时的回调函数
- */
-export const preview = ({ src, onClose }) => {
+// 定义 preview 函数的参数类型
+interface PreviewOptions {
+  src: string;
+  images?: string[];
+  onClose?: () => void;
+}
+
+export const preview = ({ src, images, onClose }: PreviewOptions) => {
   const photoGalleryContainer = document.createElement("div");
   document.body.appendChild(photoGalleryContainer);
-  const photoGallery = (
+
+  const root = createRoot(photoGalleryContainer);
+  root.render(
     <PhotoGallery
       src={src}
+      images={images}
       onClose={() => {
-        onClose && onClose();
+        if (onClose) onClose();
         setTimeout(() => {
-          root.unmount(photoGalleryContainer);
+          root.unmount();
           document.body.removeChild(photoGalleryContainer); // 清理创建的容器
         }, 300); // 退场动画刚好是0.3s
       }}
     />
   );
-  const root = createRoot(photoGalleryContainer);
-  root.render(photoGallery);
 };
